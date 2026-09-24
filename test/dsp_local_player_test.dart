45703a4177d4e165a7cdef0edbb105e30be280f5
TrapEmAll <46094434+TrapEmAll@users.noreply.github.com>
2026-09-24 05:23:15 -0400

    Test equalizer preamp plumbing

--- test/dsp_local_player_test.dart
+++ test/dsp_local_player_test.dart
@@ -7,5 +7,8 @@ void main() {
     final source = await File('lib/dsp_local_player.dart').readAsString();
     expect(source, contains('equalizer.numBands(soundHandle: handle)'));
     expect(source, contains('equalizer.bandGain(index, soundHandle: handle)'));
+    expect(source, contains('double preamp = 0'));
+    expect(source, contains('void setPreamp(double preamp)'));
+    expect(source, contains('_effectiveVolume'));
   });
 }
