0825408ee5ec9727f058b44713b8d34baad793cf
TrapEmAll <46094434+TrapEmAll@users.noreply.github.com>
2026-09-24 15:44:04 -0400

    Test sampled WAV peak detection

--- test/audio_format_info_test.dart
+++ test/audio_format_info_test.dart
@@ -5,7 +5,7 @@ import 'package:neonamp/audio_format_info.dart';
 
 void main() {
   test('parses WAV PCM format details', () {
-    final bytes = Uint8List(44)
+    final bytes = Uint8List(46)
       ..setRange(0, 4, [0x52, 0x49, 0x46, 0x46])
       ..setRange(8, 12, [0x57, 0x41, 0x56, 0x45])
       ..setRange(12, 16, [0x66, 0x6d, 0x74, 0x20]);
@@ -15,11 +15,15 @@ void main() {
     data.setUint16(22, 2, Endian.little);
     data.setUint32(24, 48000, Endian.little);
     data.setUint16(34, 24, Endian.little);
+    bytes.setRange(36, 40, [0x64, 0x61, 0x74, 0x61]);
+    data.setUint32(40, 2, Endian.little);
+    data.setUint16(42, 32767, Endian.little);
     final info = parseAudioFormat(bytes)!;
     expect(info.codec, 'WAV');
     expect(info.sampleRate, 48000);
     expect(info.bitDepth, 24);
     expect(info.channels, 2);
+    expect(info.peakDb, greaterThan(-0.1));
     expect(info.summary, contains('stereo'));
   });
 
