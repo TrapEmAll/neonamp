c09eef0c76cf52011db65765f42f30aedc1b51ab
TrapEmAll <46094434+TrapEmAll@users.noreply.github.com>
2026-09-24 15:51:59 -0400

    Use valid 24-bit WAV peak fixture

--- test/audio_format_info_test.dart
+++ test/audio_format_info_test.dart
@@ -5,7 +5,7 @@ import 'package:neonamp/audio_format_info.dart';
 
 void main() {
   test('parses WAV PCM format details', () {
-    final bytes = Uint8List(46)
+    final bytes = Uint8List(47)
       ..setRange(0, 4, [0x52, 0x49, 0x46, 0x46])
       ..setRange(8, 12, [0x57, 0x41, 0x56, 0x45])
       ..setRange(12, 16, [0x66, 0x6d, 0x74, 0x20]);
@@ -16,8 +16,8 @@ void main() {
     data.setUint32(24, 48000, Endian.little);
     data.setUint16(34, 24, Endian.little);
     bytes.setRange(36, 40, [0x64, 0x61, 0x74, 0x61]);
-    data.setUint32(40, 2, Endian.little);
-    data.setUint16(42, 32767, Endian.little);
+    data.setUint32(40, 3, Endian.little);
+    bytes.setRange(44, 47, [0xff, 0xff, 0x7f]);
     final info = parseAudioFormat(bytes)!;
     expect(info.codec, 'WAV');
     expect(info.sampleRate, 48000);
