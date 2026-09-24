68956b9bf02aaaa15a5f3908e3878011591ce14a
TrapEmAll <46094434+TrapEmAll@users.noreply.github.com>
2026-09-24 10:03:50 -0400

    Test AutoEQ filter generation

---
+++ test/parametric_eq_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:neonamp/auto_eq.dart';
+import 'package:neonamp/parametric_eq.dart';
+
+void main() {
+  test('detects custom center frequencies', () {
+    expect(hasCustomFrequencyLayout(autoEqCenterFrequencies), isFalse);
+    expect(hasCustomFrequencyLayout([20, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800]), isTrue);
+  });
+
+  test('builds an FFmpeg parametric filter chain', () {
+    final filter = buildFfmpegParametricEqFilter(frequencies: [20, 1000, 20000], gains: [3, 0, -4]);
+    expect(filter, 'equalizer=f=20:t=q:w=1:g=3,equalizer=f=20000:t=q:w=1:g=-4');
+  });
+
+  test('clamps unsafe gains and ignores invalid frequencies', () {
+    final filter = buildFfmpegParametricEqFilter(frequencies: [10, 1000, 30000], gains: [20, -20, 4]);
+    expect(filter, 'equalizer=f=1000:t=q:w=1:g=-12');
+  });
+}
